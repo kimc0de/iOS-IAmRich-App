19d73ff3d65a53bec58d5f025e102d7e6420b949
Kim Ngan <kimngan.ledang@gmail.com>
2020-11-30 01:08:50 +0100

    Initial Commit

---
+++ I-Am-Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I-Am-Rich
+//
+//  Created by Kim on 30.11.20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
